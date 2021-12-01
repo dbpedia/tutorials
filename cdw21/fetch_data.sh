e9574ccd60e2a7bca6c84ea83222c2740cc85fe8
Jan Forberg <janforberg@pop-os.localdomain>
2021-12-01 11:25:54 +0100

    tutorial resources

---
+++ cdw21/fetch_data.sh
@@ -0,0 +1,24 @@
+source helpers.sh
+
+# CREATE ANNOTATIONS
+#####################
+
+# List of documents
+documents="\
+https://raw.githubusercontent.com/dbpedia/tutorials/master/cdw21/texts/melbourne.txt \
+https://raw.githubusercontent.com/dbpedia/tutorials/master/cdw21/texts/berlin.txt \
+https://raw.githubusercontent.com/dbpedia/tutorials/master/cdw21/texts/san_francisco.txt"
+
+# Clear file
+> annotated.nt
+
+for doc in $documents; do 
+  echo "Annotating document: $doc"
+  text=$(curl -s $doc);
+  encoded=$(urlencode "$text")
+  curl -s -X GET "https://api.dbpedia-spotlight.org/en/annotate?text=$encoded&confidence=0.5" -H "accept: application/n-triples" > tmp.nt
+  sed -i -e "s#^#<$doc> #" tmp.nt
+  cat tmp.nt >> annotated.nt
+done
+
+rm tmp.nt
